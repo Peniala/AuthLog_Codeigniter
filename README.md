23f5588c7834f6d45c05258dc8f63793aa753bba
Peniala <peniala.ranaivo@gmail.com>
2024-05-03 00:02:07 +0300

    Upgrade sendmail

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Modification nécessaire :
 - l'utilisateur et le mot de passe pour se connecter à la base de donnée sur le script auth.php ( valeur par défaut : "root" , "" );
 
 #Crontab
-- le chemin vers l'emplacement du script PHP;
+- le chemin vers l'emplacement des scripts PHP sur l'actualisation de la base de donnée et l'envoie du mail;
 
 #Uniformisation de la syntaxe du fichier auth.log
 - ajout du template 10-mytemplate.conf dans /etc/rsyslog.d
--- script-mail.php
+++ script-mail.php
@@ -8,19 +8,45 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer(true);
 
 try{
+    // Recuperation des donnees
 
-    $sql = new mysqli('localhost','mit','123456','mit');
+    $sql = new mysqli('localhost','mit','123456','mit') or die("Erreur de connexion");
     
-    $table = $sql->query("select distinct s.hostname from session right join dhcp on s.hostname = d.ip where date like '".date('Y-m-d',time())."%'");
+    $request = "select distinct ip,user,id_machine,grade,niveau,nom,prenoms,type,date from session 
+    right join dhcp on hostname = ip and session.date like '".date('Y-m-d',time())."%' and type = 'opened' 
+    left join machine_etudiants on id_machine_etudiant = id_machine 
+    inner join inscription on machine_etudiants.id_inscription = inscription.id_inscription 
+    inner join etudiants on etudiants.id_etudiant = inscription.id_etudiant 
+    inner join personnes on personnes.id_personne = etudiants.id_personne";
+
+    $table = $sql->query($request);
+    $i = 1;
+    
+    $users[0]['name'] = "Nom";
+    $users[0]['level'] = 'Niveau';
+    $users[0]['ip'] = 'IP';
+    $users[0]['status'] = 'Connexion';
 
     while($u = mysqli_fetch_assoc($table)){
-        $users[]['name'] = $u['hostname'];
-        // $users[]['level'] = $u['level'];
-        $users[]['status'] = ($u)'connected'; 
+        $users[$i]['name'] = $u['nom']." ".$u['prenoms'];
+        $users[$i]['level'] = $u['grade'].$u['niveau'];
+        $users[$i]['ip'] = $u['ip'];
+        $users[$i]['status'] = ($u['type'] == null) ? 'Déconnecté' : 'Connecté'; 
+        $i++;
     }
 
-    print_r($users);
-    /// Config
+    /// Creation du fichier csv
+
+    $f = __DIR__.'/connexion.csv';
+    $file = fopen($f, 'w');
+
+    echo $f.'<br>';
+    foreach ($users as $row) {
+        fputcsv($file, $row);
+    }
+    fclose($file);
+
+    /// Config du Mail avec SMTP
     
     $mail->isSMTP();
     $mail->SMTPAuth = true;
@@ -33,21 +59,21 @@ try{
     /// Destinataire
 
     $mail->setFrom('pranaivo@mit-ua.mg');
-    //$mail->addReplyTo('pranaivo@mit-ua.mg','Peniala');
+    $mail->addReplyTo('arakotoarijaona@mit-ua.mg');
 
-    $mail->addAddress('arakotoarijaona@mit-ua.mg');
+    // $mail->addAddress('arakotoarijaona@mit-ua.mg'); Cc
 
     /// Rattacher un fichier
 
-    $mail->Subject = "Test 3 SendMail";
+    $mail->Subject = "Liste des connexions du ".date('Y-m-d',time());
 
-    $mail->Body = "Envoie du mail reussi";
+    $mail->Body = "Monsieur, Voici le fichier joint qui contient la liste des connexions à ce jour.";
 
-    $mail->addAttachment("./auth.php");
+    $mail->addAttachment($f);
 
-    /// Envoi du mail
+    // Envoi du mail
 
-    // $mail->send();
+    $mail->send();
 
 }
 catch(Exception $exception){
